0ce9c78dbab576f17a7dd35228b1939b5fb82335
mohamed <mohamed.abdisalam@gmail.com>
2023-11-14 17:29:08 +0000

    Present tests DONE

---
+++ tests/test_present.py
@@ -0,0 +1,54 @@
+import pytest
+from lib.present import *
+
+"""
+Test for wrapping and unwrapping
+Happy path / case
+"""
+
+def test_wrap_then_unwrap():
+    present = Present()
+    present.wrap("toys")
+    assert present.unwrap() == "toys"
+
+"""
+When there is already a present
+The user should see following message:
+"A contents has already been wrapped."
+"""
+
+def test_present_already_wrapped():
+    present = Present()
+    present.wrap("toys")
+    with pytest.raises(Exception) as e:
+        present.wrap("toy_car")
+    error_message = str(e.value) # <-- New code
+    assert error_message == "A contents has already been wrapped."
+
+"""
+If there is not a present 
+and you try to unwrap it
+The user should see following message:
+"No contents have been wrapped."
+"""
+
+def test_no_present_to_unwrap():
+    present = Present()
+    with pytest.raises(Exception) as e:
+        present.unwrap()
+    error_message = str(e.value) # <-- New code
+    assert error_message == "No contents have been wrapped."
+
+"""
+If a present has already been wrapped
+And you try to wrap another present
+When you unwrap it should return the original present
+"""
+
+def test_double_wrap_then_unwrap():
+    present = Present()
+    present.wrap("toys")
+    with pytest.raises(Exception) as e:
+        present.wrap("toy_car")
+    assert present.unwrap() == "toys"
+
